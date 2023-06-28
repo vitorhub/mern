4d4e7f649f148014fd8c0dabc3045f85771dc3d8
vitor falcao <joaofalcao.33@outlook.com>
2023-06-27 21:03:27 -0300

    added likes

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,215 +1,250 @@
 import {
-    createService,
-    findAllService,
-    countNews,
-    topNewsService,
-    findByIdService,
-    searchByTitleService,
-    byUserService,
-    updateService,
-} from '../services/news.service.js'
+  createService,
+  findAllService,
+  countNews,
+  topNewsService,
+  findByIdService,
+  searchByTitleService,
+  byUserService,
+  updateService,
+  eraseService,
+  likeNewsService,
+  deleteLikeNewsService,
+} from "../services/news.service.js";
 
 const create = async (req, res) => {
-    try {
-        const { title, text, banner } = req.body
+  try {
+    const { title, text, banner } = req.body;
 
-        if (!title || !text || !banner) {
-            res.status(400).send({
-                message: "Submit all fields for registration"
-            })
-        }
-
-        await createService({
-            title,
-            text,
-            banner,
-            user: req.userId
-        })
-
-        res.send(201)
-    } catch (err) {
-        res.status(500).send({ message: err.message })
+    if (!title || !text || !banner) {
+      res.status(400).send({
+        message: "Submit all fields for registration",
+      });
     }
-}
+
+    await createService({
+      title,
+      text,
+      banner,
+      user: req.userId,
+    });
+
+    res.send(201);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 const findAll = async (req, res) => {
+  try {
+    let { limit, offset } = req.query;
+    limit = Number(limit);
+    offset = Number(offset);
 
-    try {
-        let { limit, offset } = req.query
-        limit = Number(limit)
-        offset = Number(offset)
-    
-        if (!limit) {
-            limit = 5
-        }
-    
-        if (!offset) {
-            offset = 0
-        }
-    
-        const news = await findAllService(offset, limit)
-        const total = await countNews()
-        const currentUrl = req.baseUrl
-    
-        const next = offset + limit
-        const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null
-    
-        const previous = offset - limit < 0 ? null : offset - limit
-        const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null
-    
-        if (news.lenght === 0) {
-            return res.status(400).send({ message: "There are no news" }) // se nenhum usuario para aqui   
-        }
-        res.send({
-            nextUrl,
-            previous,
-            limit,
-            offset,
-            total,
-            results: news.map(item => ({
-                id: item._id,
-                title: item.title,
-                text: item.text,
-                banner: item.banner,
-                likes: item.likes,
-                comments: item.comments,
-                name: item.user.name,
-                username: item.user.username,
-                userAvatar: item.user.avatar,
-            }))
-        })
-    } catch (err) {
-        res.status(500).send({ message: err.message })
+    if (!limit) {
+      limit = 5;
     }
-}
+
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
+    if (news.lenght === 0) {
+      return res.status(400).send({ message: "There are no news" }); // se nenhum usuario para aqui
+    }
+    res.send({
+      nextUrl,
+      previous,
+      limit,
+      offset,
+      total,
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
 const topNews = async (req, res) => {
-    try {
-        const news = await topNewsService()
-    
-        if (!news) {
-            return res.status(400).send({ message: 'There is no registered post' })
-        }
-    
-        res.send({
-            news: {
-                id: news._id,
-                title: news.title,
-                text: news.text,
-                banner: news.banner,
-                likes: news.likes,
-                comments: news.comments,
-                name: news.user.name,
-                username: news.user.username,
-                userAvatar: news.user.avatar,
-            }
-        })
-        
-    } catch (err) {
-        res.status(500).send({ message: err.message })
+  try {
+    const news = await topNewsService();
+
+    if (!news) {
+      return res.status(400).send({ message: "There is no registered post" });
     }
-}
+
+    res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        username: news.user.username,
+        userAvatar: news.user.avatar,
+      },
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
 const findById = async (req, res) => {
-    try {
-        const { id } = req.params
+  try {
+    const { id } = req.params;
 
-        const news = await findByIdService(id)
+    const news = await findByIdService(id);
 
-        return res.send({
-            news: {
-                id: news._id,
-                title: news.title,
-                text: news.text,
-                banner: news.banner,
-                likes: news.likes,
-                comments: news.comments,
-                name: news.user.name,
-                username: news.user.username,
-                userAvatar: news.user.avatar,
-            }
-        })
-    } catch (err) {
-        res.status(500).send({ message: err.message })
-    }
-}
+    return res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        username: news.user.username,
+        userAvatar: news.user.avatar,
+      },
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
 export const searchByTitle = async (req, res) => {
-    try {
-        const {title} = req.query
+  try {
+    const { title } = req.query;
 
-        const news = await searchByTitleService(title)
+    const news = await searchByTitleService(title);
 
-        if (news.lenght === 0){
-            return res
-            .status(400)
-            .send({message: "There are no news with this title"})
-        }
-        return res.send({
-            results: news.map(item => ({
-                id: item._id,
-                title: item.title,
-                text: item.text,
-                banner: item.banner,
-                likes: item.likes,
-                comments: item.comments,
-                name: item.user.name,
-                username: item.user.username,
-                userAvatar: item.user.avatar,
-            })),
-        })
-    } catch (error) {
-        res.status(500).send({message: err.message})
+    if (news.lenght === 0) {
+      return res
+        .status(400)
+        .send({ message: "There are no news with this title" });
     }
-}
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (error) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
-export const byUser = async (req, res)=>{
-    try {
-        const id = req.userId
-        const news = await byUserService(id)
+export const byUser = async (req, res) => {
+  try {
+    const id = req.userId;
+    const news = await byUserService(id);
 
-        return res.send({
-            results: news.map(item => ({
-                id: item._id,
-                title: item.title,
-                text: item.text,
-                banner: item.banner,
-                likes: item.likes,
-                comments: item.comments,
-                name: item.user.name,
-                username: item.user.username,
-                userAvatar: item.user.avatar,
-            })),
-        })
-
-    } catch (err) {
-        res.status(500).send({message: err.message})
-    }
-}
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
 export const update = async (req, res) => {
-    try {
-        const {title, text, banner} = req.body
-        const {id} = req.params
-        if(!title && !banner && !text ){
-            res.status(400).send({
-                message: "Submit at least on field to update the post"
-            })
-        }
-        const news = await findByIdService(id)
-        if(news.user._id != req.userId){
-            return res.status(400).send({
-                message: "you didnt update this post"
-            })
-        }
-        await updateService(id, title, text, banner)
-        return res.send({message: "post sucessfully updated"})
-    } catch (err) {
-        res.status(500).send({ message: err.message})
+  try {
+    const { title, text, banner } = req.body;
+    const { id } = req.params;
+    if (!title && !banner && !text) {
+      res.status(400).send({
+        message: "Submit at least on field to update the post",
+      });
     }
-}
+    const news = await findByIdService(id);
+    if (news.user._id != req.userId) {
+      return res.status(400).send({
+        message: "you didnt update this post",
+      });
+    }
+    await updateService(id, title, text, banner);
+    return res.send({ message: "post sucessfully updated" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
 export const erase = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const news = await findByIdService(id);
+    if (news.user._id != req.userId) {
+      return res.status(400).send({
+        message: "you didnt delete this post",
+      });
+    }
+    await eraseService(id);
+    return res.send({ message: "News deleted successfully" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
-}
+export const likeNews = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.userId;
 
-export { create, findAll, topNews, findById }
+    const newsLiked = likeNewsService(id, userId);
+    
+    if(!newsLiked){
+        await deleteLikeNewsService(id, userId)
+        return res.status(200).send({message: "like sussessfully removed"})
+    }
+
+    res.send("ok");
+
+  } catch (err) {
+    res.status(500).send({message: err.message})
+  }
+};
+
+export { create, findAll, topNews, findById };
--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 const router = Router()
 
-import { create, findAll, topNews, findById, searchByTitle, byUser, update, erase } from '../controllers/news.controller.js'
+import { create, findAll, topNews, findById, searchByTitle, byUser, update, erase, likeNews } from '../controllers/news.controller.js'
 import { authMiddleware } from '../middlewares/auth.middleware.js'
 
 router.post("/", authMiddleware, create) // rota, controler chamado create
@@ -12,5 +12,6 @@ router.get("byUser",  authMiddleware, byUser)
 router.get("/:id", authMiddleware, findById) // rota e função de callback
 router.patch("/:id", authMiddleware, update)
 router.delete("/:id", authMiddleware, erase)
+router.patch("/like/:id" , authMiddleware, likeNews)
 
 export default router
